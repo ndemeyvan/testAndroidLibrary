448b72c072e9609e9d1d65945ed46f85f3ea776a
ndeme yvan <yvan.ndeme@papersoft.ng>
2021-05-17 12:26:43 +0100

    commit toast library:

--- LogUtil/src/main/java/com/example/logutil/LogDebug.kt
+++ LogUtil/src/main/java/com/example/logutil/LogDebug.kt
@@ -1,11 +1,12 @@
 package com.example.logutil
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
 
-
-public class LogDebug {
-
-  public fun makeLogger(message: String){
-      Log.d("LogUtil",message)
-  }
-
+object LogDebug {
+    public  fun makeLogger(context : Context, message: String){
+        Log.d("LogUtil",message)
+        Toast.makeText(context,message,LENGTH_LONG).show()
+    }
 }
--- app/src/main/java/com/example/loguiltexample/MainActivity.kt
+++ app/src/main/java/com/example/loguiltexample/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.loguiltexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.logutil.LogDebug
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toastButton.setOnClickListener {
+            LogDebug.makeLogger(this, "Hello from log debug")
+        }
+
     }
 }
